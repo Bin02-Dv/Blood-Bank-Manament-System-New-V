9a4745b5af6900de46ab0ba6aef876b7078bc017
Bin02-Dv <alaminbinahmad@gmail.com>
2025-11-25 10:05:46 -0800

    first commit

---
+++ BBMSProject/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for BBMSProject project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BBMSProject.settings')
+
+application = get_asgi_application()
