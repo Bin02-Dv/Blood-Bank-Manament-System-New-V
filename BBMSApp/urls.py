6346996d7f7ee20abca4206b9e638a450b7349a0
Bin02-Dv <alaminbinahmad@gmail.com>
2025-12-22 15:56:03 -0800

    12/22/2025 - new design structure

--- BBMSApp/urls.py
+++ BBMSApp/urls.py
@@ -3,13 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # admin
-    path("admin/dash/", views.admin_dash, name="admin_dash"),
-    
-    # donor
-    path("donor/dash/", views.donor_dash, name="donor-dash"),
-    
-    # auth
-    path("login/", views.login, name="login"),
-    path("register/", views.register, name="register")
+    path("", views.index, name="index")
 ]
