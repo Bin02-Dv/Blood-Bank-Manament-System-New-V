ef59b401d44d6ed1dcf4f54724d81214bf84c3e5
Bin02-Dv <alaminbinahmad@gmail.com>
2025-12-23 11:20:24 -0800

    The landing page is completed

--- BBMSApp/urls.py
+++ BBMSApp/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("signup/", views.signup, name="signup"),
+    path("login/", views.login, name="login"),
+    path("contact/", views.contact_us, name="contact"),
+    path("404/", views.page_404, name="404"),
+    path("500/", views.page_500, name="500")
 ]
--- BBMSApp/views.py
+++ BBMSApp/views.py
@@ -4,3 +4,21 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "index.html")
+
+def signup(request):
+    return render(request, "signup.html")
+
+def login(request):
+    return render(request, "login.html")
+
+def contact_us(request):
+    return render(request, "contactUs.html")
+
+
+# error handler
+
+def page_404(request, exception):
+    return render(request, "404.html")
+
+def page_500(request, exception):
+    return render(request, "500.html")
