eaedfd24fe5216fa9f09d7a0a8e4ffd115531145
Bin02-Dv <alaminbinahmad@gmail.com>
2025-12-22 16:23:59 -0800

    4:23pm

--- BBMSProject/settings.py
+++ BBMSProject/settings.py
@@ -78,13 +78,27 @@ WSGI_APPLICATION = 'BBMSProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DATABASE"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST"),
+        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
     }
 }
 
+# Only require SSL if NOT local
+if os.environ.get("POSTGRES_HOST") and os.environ.get("POSTGRES_HOST") != "localhost":
+    DATABASES["default"]["OPTIONS"] = {"sslmode": "require"}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
